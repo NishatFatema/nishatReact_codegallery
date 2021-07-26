85375288345b028b7e7ce202fa21156f03cef211
NishatFatema <86831426+NishatFatema@users.noreply.github.com>
2021-07-26 23:15:47 +0530

    all app related

--- e-learn/src/App1.js
+++ e-learn/src/App1.js
@@ -1,15 +1,12 @@
 import './App.css';
-import Elearn from "./components/Elearn"
-import  Elearn1  from './components1/Elearn1';
+import { Elearn1 } from "./components1/Elearn1"
 
 
 function App() {
   return (
     <div className="App">
         
-      <Elearn/>
       <Elearn1/>
-
 </div>
   );
 }
---
+++ e-learn/src/components1/Elearn1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Elearn1.css";
+export const Elearn1 = () => {
+    return  (
+                <div>
+
+<marquee className="marquee" behavior="scroll" direction="left"><b>www.pickupbiz.co.in</b>
+
+</marquee>
+                </div>
+
+             )
+
+}
+export default Elearn1
