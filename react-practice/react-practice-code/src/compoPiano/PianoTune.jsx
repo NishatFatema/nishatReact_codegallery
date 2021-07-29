148d92e292faab683c9e168996bcbe2d3ea6d979
NishatFatema <86831426+NishatFatema@users.noreply.github.com>
2021-07-29 16:20:31 +0530

    piano code with keyboard

--- react-practice/react-practice-code/src/compoPiano/PianoTune.jsx
+++ react-practice/react-practice-code/src/compoPiano/PianoTune.jsx
@@ -6,19 +6,43 @@ export const PianoTune = ()=>{
     const handleplay = (bit) =>{
     const audio = new Audio(`./MP3/${bit}.mp3`);
         audio.play();
-    }
+    };
+
+    document.addEventListener("keydown",(e)=>{
+
+        
+
+        if (e.key==="a" && handleplay("A"));
+        if (e.key==="s" && handleplay("B"));
+        if (e.key==="d" && handleplay("C"));
+        if (e.key==="f" && handleplay("D"));
+        if (e.key==="g" && handleplay("E"));
+        if (e.key==="h" && handleplay("F"));
+        if (e.key==="j" && handleplay("G"));
+        if (e.key==="A" && handleplay("Ab"));
+        if (e.key==="S" && handleplay("Bb"));
+        if (e.key==="D" && handleplay("C"));
+        if (e.key==="F" && handleplay("Db"));
+        if (e.key==="G" && handleplay("Eb"));
+        if (e.key==="H" && handleplay("F"));
+        if (e.key==="J" && handleplay("Gb"));
+        
+        
+        e.stopPropagation();
+        
+      })
     
     return(
         <div>
 
-            <Button variant="outline-secondary" onClick={()=>handleplay("A")} >Sa</Button>
-            <Button variant="outline-secondary" onClick={()=>handleplay("B")}>Re</Button>
-            <Button variant="outline-secondary" onClick={()=>handleplay("C")}>Ga</Button>
-            <Button variant="outline-secondary" onClick={()=>handleplay("D")}>Ma</Button>
-            <Button variant="outline-secondary" onClick={()=>handleplay("E")}>Pa</Button>
-            <Button variant="outline-secondary" onClick={()=>handleplay("F")}>Dha</Button>
-            <Button variant="outline-secondary" onClick={()=>handleplay("G")}>ni</Button>
-            <Button variant="outline-secondary" onClick={()=>handleplay("A")}>Sa</Button>
+            <Button variant="outline-dark" className="Sa" onClick={()=>handleplay("A")} >Sa</Button>
+            <Button variant="outline-dark" className="Re" onClick={()=>handleplay("B")}>Re</Button>
+            <Button variant="outline-dark" className="Ga" onClick={()=>handleplay("C")}>Ga</Button>
+            <Button variant="outline-dark" className="Ma"  onClick={()=>handleplay("D")}>Ma</Button>
+            <Button variant="outline-dark" className="Pa" onClick={()=>handleplay("E")}>Pa</Button>
+            <Button variant="outline-dark" className="Dha" onClick={()=>handleplay("F")}>Dha</Button>
+            <Button variant="outline-dark" className="ni" onClick={()=>handleplay("G")}>ni</Button>
+            <Button variant="outline-dark" className="Sa"  onClick={()=>handleplay("A")}>Sa</Button>
      
 
         </div>
