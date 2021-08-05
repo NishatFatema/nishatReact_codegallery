268d3af695be7dfc29f940f74b01cdfea69cf120
NishatFatema <86831426+NishatFatema@users.noreply.github.com>
2021-08-05 11:57:36 +0530

    server created code

---
+++ APIBuilder/asyncway.js
@@ -0,0 +1,8 @@
+const asycnway = () =>{
+
+    setTimeout(()=>{
+          console.log.apply("first call");
+                   },4000);
+
+                   console.log("second call");
+                  }
