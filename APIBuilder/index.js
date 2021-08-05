eb68011cd2d0232cb37dab959c6fb9efd683d13b
NishatFatema <86831426+NishatFatema@users.noreply.github.com>
2021-08-05 12:02:12 +0530

    async call code

--- APIBuilder/index.js
+++ APIBuilder/index.js
@@ -1,6 +1,24 @@
-const express = require("express")
+const express=require ("express");
 const app=express();
 
-if (app.listen(4000)){
-    console.log("connected to local host:4000")
+app.get("/",(req,res)=>{
+
+   res.json({data:["umar","uzair","zoha","bushra"]})
+
+        })
+
+    app.get("/emp",(req,res)=>{
+
+            res.send("we learning with express")
+
+        })
+            app.get("/dept",(req,res)=>{
+
+                res.send("we are completing our projects")
+
+            })
+
+if (app.listen(2000)){
+    console.log("connected to local host:2000")
 }
+
